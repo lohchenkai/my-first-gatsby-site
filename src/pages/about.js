400029724bbf562fa8cc64f0ff223cfa5ac06d3a
Chen Kai <lohchenkai@gmail.com>
2022-09-17 00:56:27 +0800

    added about page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function About(props){
+    return (
+        <main>
+            <h1>About Me</h1>
+            <p>LOH currently in the process of learning how to use the Gatsby Frameworks</p>
+        </main>
+    )
+}
+
+export const Head = () => <title>About Me</title>
