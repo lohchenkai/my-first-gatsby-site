f13f01526dc6134901efbcf068d7c642670adf6d
lohchenkai <lohchenkai@gmail.com>
2022-10-02 22:58:09 +0800

    Finished Gatsby Tutorial Part 3

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Layout from '../components/layout'
+import { StaticImage } from "gatsby-plugin-image"
 
 export default function About(){
     return (
         <Layout pageTitle="Here's a Page About Myself">
             <p>I'm currently in the process of learning how to use the Gatsby Frameworks</p>
+            <StaticImage src="../images/chi-chi.jpg" alt="chi-chi" />
         </Layout>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from "gatsby-plugin-image"
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Welcome to My Gatsby Site">
-      <p>I'm making this by following the Gatsby Tutorial.</p>    
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage src="https://images.unsplash.com/photo-1589210043112-d4835d91b37a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80" alt="shih-tzu" />
     </Layout>
   )
 }
